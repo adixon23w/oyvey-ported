75a261da086ddcaa814e71888b5d9d3f7f59926e
adixon <adixon23w@gmail.com>
2025-12-16 16:50:50 +0100

    Update Reach.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Reach.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Reach.java
@@ -3,6 +3,7 @@ package me.alpha432.oyvey.features.modules.combat;
 import me.alpha432.oyvey.event.impl.PacketEvent;
 import me.alpha432.oyvey.event.system.Subscribe;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.setting.Setting;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.world.entity.Entity;
@@ -11,18 +12,19 @@ import net.minecraft.world.entity.boss.enderdragon.EndCrystal;
 
     
 public class Reach extends Module {
-    public Reach() {
-        super("Reach", Category.COMBAT);
-    }
-
+    
     public final Setting<Float> blocksRange = new Setting<>("BlocksRange", 3f, 0.1f, 10.0f);
     public final Setting<Float> entityRange = new Setting<>("EntityRange", 3f, 0.1f, 10.0f);
     public final Setting<Boolean> Creative = new Setting<>("Creative", false);
     public final Setting<Float> creativeBlocksRange = new Setting<>("CBlocksRange", 5f, 0.1f, 10.0f, v -> Creative.getValue());
     public final Setting<Float> creativeEntityRange = new Setting<>("CEntityRange", 5f, 0.1f, 10.0f, v -> Creative.getValue());
+    
+    public Reach() {
+        super("Reach", Category.COMBAT);
+    }
 
     @Override
     public String getDisplayInfo() {
-        return "B: " + blocksRange.getValue() + " E:" + entityRange.getValue();
+        return "B: " + blocksRange.getValue() + " E: " + entityRange.getValue();
     }
 }
